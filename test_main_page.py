cf5dec2941259e13c4f36c9eb0dc76a8ba7a68e3
pokeyrover <pokeyrover@gmail.com>
2022-07-03 23:50:48 +0300

    update is_present mehtods in page classes and main page test

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
+
 
 
 def test_guest_can_go_to_login_page(browser):
     browser, link = browser
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, '#login_link')
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
+    
