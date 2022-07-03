a9af573e24c0ccb0c5374a25da2058eda90e2724
pokeyrover <pokeyrover@gmail.com>
2022-07-04 01:06:34 +0300

    comments update

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,15 +2,15 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
-        self.browser = browser
+    def __init__(self, browser, url, timeout=10):       #initiate page object
+        self.browser = browser                          
         self.url = url
         self.browser.implicitly_wait(timeout)
         
-    def open(self):
+    def open(self):                                     #open page function
         self.browser.get(self.url)
         
-    def is_element_present(self, how, what):
+    def is_element_present(self, how, what):            #element verification function
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,15 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 
-class MainPage(BasePage):
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, '#login_link')
+
+class MainPage(BasePage, MainPageLocators):
+    def go_to_login_page(self):                                                                         #open login page function
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
         
-    def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, '#login_link'), \
+    def should_be_login_link(self):                                                                     #verification of login link
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), \
             'Login link is not present'
             
