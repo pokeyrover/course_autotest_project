2d2705afb9936ce863df31199f402f919969d25c
pokeyrover <pokeyrover@gmail.com>
2022-07-04 17:55:14 +0300

    relocate get link variable function to locators.py

--- conftest.py
+++ conftest.py
@@ -5,34 +5,29 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en', \
-        help='Choose browser language in two letters format')
+        help='Choose browser language in two letters format')                               #Adding choosing browser language option by --language
     parser.addoption('--browser_name', action='store', default='chrome',\
-        help='Choose browser: chrome or firefox')
+        help='Choose browser: chrome or firefox')                                           #Adding choosing browser option by --browser_name
     
 @pytest.fixture(scope='function')
-def browser(request):
+def browser(request):                                                                       #function for open choosed browser for each test
     browser_name = request.config.getoption('browser_name')
     browser = None
     
-    link = 'http://selenium1py.pythonanywhere.com/'
-
     user_language = request.config.getoption('language')
     if len(user_language) != 2:
         raise pytest.UsageError('--language should content two letters')
     
-    if browser_name.lower() == 'chrome':
+    if browser_name.lower() == 'chrome':                                                    #will open chrome
         print('\nstart chrome for test..')
-        browser = webdriver.Chrome()
         
         options = Options()
         
-        options.add_experimental_option('prefs', {'intl.accepr_languages': \
-            user_language})
+        options.add_experimental_option('prefs', {'intl.accepr_languages': user_language})
         browser = webdriver.Chrome(options=options)
         
-    elif browser_name.lower() == 'firefox':
+    elif browser_name.lower() == 'firefox':                                                 #will open firefox
         print('\nstart firefox to test..')
-        browser = webdriver.Firefox()
         
         fire_prof = webdriver.FirefoxProfile()
         fire_prof.set_preference('intl.accept_languages', user_language)
@@ -41,7 +36,7 @@ def browser(request):
     else:
         raise pytest.UsageError('--browser_name was wrong, should be \
             chrome or firefox')
-    yield browser, link
-    print('\nquit browser')
+    yield browser
+    print('\nquit browser')                                                                 #browser will close after each test
     browser.quit()
     
