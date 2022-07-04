01487b99e4a230fcc0b27310004963aac0fe34d0
pokeyrover <pokeyrover@gmail.com>
2022-07-04 18:05:35 +0300

    add get_text_from_element function and sulution for math exercise from add product to basket alert

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
+import math
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
 
 
 class BasePage():
@@ -16,4 +18,22 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+    
+    def get_element_text(self, how, what):              #function for getting text from element
+        element = self.browser.find_element(how, what)
+        return element.text
+        
+    def solve_quiz_and_get_code(self):                  #function for solution for a math example in add product alert
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(' ')[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f'Your code: {alert_text}')
+            alert.accept()
+        except NoAlertPresentException:
+            print('No secoond alert presented')
         
