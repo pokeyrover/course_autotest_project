8431ac8b3f7d3ebbfebb31650b0398fc75b0d521
pokeyrover <pokeyrover@gmail.com>
2022-07-05 15:18:19 +0300

    add check login link and go to login page tests in test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,9 @@
 import pytest
 from .pages.product_page import ProductPage
-from .pages.locators import ProductPageLocators
+from .pages.login_page import LoginPage
 
+
+@pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -14,7 +16,7 @@ from .pages.locators import ProductPageLocators
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"    
 ])
-def test_guest_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket(browser, link):                                            #will add product to basket and check success messages
     browser = browser
     page = ProductPage(browser, link)
     page.open()
@@ -23,3 +25,39 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.basket_price_is_correct()
     page.product_name_is_correct()
     
+def test_guest_should_see_login_link_on_product_page(browser):                                      #checking present link to login page
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    
+def test_guest_can_go_to_login_page_from_product_page(browser):                                     #checking possibility to open login page from product page
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_product_to_basket(browser):                           #will check not present a success messages after adding product in basket
+    browser = browser
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_succes_message()
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message(browser):                                                   #will check not present a success message before add product
+    browser = browser
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_succes_message()
+
+@pytest.mark.skip
+def test_message_disappeared_after_adding_product_to_basket(browser):                               #will check disappearing a success message after add product
+    browser = browser
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_disappear()
