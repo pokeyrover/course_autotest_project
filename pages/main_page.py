f4da5fb700ad2ad8d864070e269ac8daa1d6d1ee
pokeyrover <pokeyrover@gmail.com>
2022-07-06 16:00:58 +0300

    add go to basket page when basket empty test from main page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,9 @@
+from .basket_page import BasketPage
 from .base_page import BasePage
 from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 
-class MainPage(BasePage, MainPageLocators):
+class MainPage(BasketPage, BasePage, MainPageLocators):
     pass
             
--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,20 @@
+import pytest
+from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 from .pages.locators import MainPageLocators
 
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_go_to_login_page(browser):                                                   #check possibility to go on login page from main page
     browser = browser
     page = MainPage(browser, MainPageLocators.PAGE_URL)
     page.open()
-    page.go_to_login_page()
     page.should_be_login_link()
+    page.go_to_login_page()
     
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):                       #check possibility to go to basket page from main page when basket is empty
+    browser = browser
+    link = MainPageLocators.PAGE_URL
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.basket_should_empty()
